9a47b9e512c0688e017331609d16c63f00b5b962
Paul Boersma <pcboersma91@gmail.com>
2022-09-16 12:30:07 +0200

    handle intro story

--- cyoa/main.go
+++ cyoa/main.go
@@ -1,36 +1,42 @@
 package main
 
 import (
-	"encoding/json"
 	"flag"
 	"fmt"
 	"log"
+	"net/http"
 	"os"
 	"path"
+	"text/template"
 
 	"github.com/Paul-Boersma/gophercises/cyoa/model"
 )
 
 func main() {
+	// Command line input
 	filename := flag.String("file", "./stories/gopher.json", "Choose a .json story")
 	flag.Parse()
 	storyname := path.Base(*filename)[:1+len(path.Ext(*filename))]
 
-	fmt.Printf("Welcome to story: %s\n", storyname)
+	fmt.Printf("Welcome to story: %s\n\n", storyname)
 
+	// Json File
 	file, err := os.Open(*filename)
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer file.Close()
 
-	var story model.Story
-	decoder := json.NewDecoder(file)
-	if err := decoder.Decode(&story); err != nil {
+	// Go Struct
+	story, err := model.JsonStory(file)
+	if err != nil {
 		log.Fatal(err)
 	}
 
+	// WebTemplate
 	fmt.Println(story)
+	tmpl := template.Must(template.ParseFiles("./templates/cyoa.gohtml"))
+	tmpl.Execute(os.Stdout, story)
 
 	// Parse the byteslice throught json.Unmarshal()
 	// Create a template file
@@ -38,4 +44,24 @@ func main() {
 	// Set up a simple server and client
 	// When a user makes a web request send the template as a response.
 
+	// Setup webserver
+	handlerCyoa := NewHandler(tmpl, story)
+	http.HandleFunc("/cyoa", handlerCyoa.ServeHTTP)
+	http.ListenAndServe(":8080", nil)
+}
+
+func NewHandler(tmpl *template.Template, story model.Story) CyoaHTTPHandler {
+	return CyoaHTTPHandler{
+		story: story,
+		tmpl:  tmpl,
+	}
+}
+
+type CyoaHTTPHandler struct {
+	story model.Story
+	tmpl  *template.Template
+}
+
+func (h *CyoaHTTPHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	h.tmpl.Execute(w, h.story["intro"])
 }
--- cyoa/model/story.go
+++ cyoa/model/story.go
@@ -1,5 +1,10 @@
 package model
 
+import (
+	"encoding/json"
+	"io"
+)
+
 type Story map[string]Chapter
 
 type Chapter struct {
@@ -11,4 +16,10 @@ type Chapter struct {
 type Option struct {
 	ChapterReference string `json:"arc"`
 	Text             string `json:"text"`
+}
+
+func JsonStory(r io.Reader) (story Story, err error) {
+	decoder := json.NewDecoder(r)
+	err = decoder.Decode(&story)
+	return
 }
